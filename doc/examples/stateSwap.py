078ab5412701db4bd2bb42b9a05e1ad486eb3021
Alexei Gilchrist <alexei.gilchrist@mq.edu.au>
2002-11-21 23:51:42 +0000

    Some rearangeing of files:
    /doc
    /doc/examples
    
    /libs (object libraries)
    /pyscript (base package)

---
+++ doc/examples/stateSwap.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# State swapping quantum computing circuit diagram
+
+import sys
+sys.path.append('../')
+sys.path.append('../pyscript')
+
+from pyscript import *
+from pyscriptQuantInfo import *
+
+#----------------------------------------------------------------------------------------------
+defaults.units=UNITS['cm']
+
+defaults.tex_head=r"""
+\documentclass{article}
+\pagestyle{empty}
+
+\newcommand{\ket}[2]{\mbox{$|#1\rangle_{#2}$}}
+\newcommand{\bra}[1]{\mbox{$\langle #1|$}}
+\newcommand{\braket}[2]{\mbox{$\langle #1|#2\rangle$}}
+\newcommand{\op}[1]{\mbox{\boldmath $\hat{#1}$}}
+\begin{document}
+"""
+
+# define the rails
+rail1 = Rail(w=P(0,0), length=3.0, labelIn=r'\ket{\psi}{}', labelOut=r'\ket{\phi}{}')
+rail2 = Rail(w=P(0,1), length=3.0, labelIn=r'\ket{\phi}{}', labelOut=r'\ket{\psi}{}')
+
+# three controlled nots
+cnot1 = Cnot(c=P(0.5,0), targetDist=1.0, direction="up")
+cnot2 = Cnot(c=P(1.5,1), targetDist=1.0, direction="down")
+cnot3 = Cnot(c=P(2.5,0), targetDist=1.0, direction="up")
+
+# draw it!
+render("stateSwap.eps",rail1,rail2,cnot1,cnot2,cnot3)
+
