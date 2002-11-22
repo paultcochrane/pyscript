5a841b7cfb038c92364cc1228580da79413659c7
Alexei Gilchrist <alexei.gilchrist@mq.edu.au>
2002-11-22 01:22:19 +0000

    These two files are for distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="pyscript",
+      version="0.1",
+      description="Python Postscript Scripting",
+      author="Alexei Gilchrist and Paul Cochrane",
+      author_email="aalexei@sourceforge.net",
+      url="http://pyscript.sourceforge.net",
+      packages=['pyscript','pyscript.lib'],
+)
