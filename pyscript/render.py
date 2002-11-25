173f55f392f99a17328fbca78a8c1a359b88b025
Alexei Gilchrist <alexei.gilchrist@mq.edu.au>
2002-11-25 02:12:42 +0000

    updated the copyright notice

--- pyscript/render.py
+++ pyscript/render.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002  Alexei Gilchrist
+# Copyright (C) 2002  Alexei Gilchrist and Paul Cochrane
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
