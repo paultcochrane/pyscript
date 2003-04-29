0d502aff8b683d076dffd03d950374fafe8fc8c3
Paul Cochrane <paul@liekut.de>
2003-04-29 06:11:03 +0000

    Added many more named colours to hash of defined colours.  All names are in lower case, but users can use upper and lower mixed.  Have imported the string module to make sure that any uppercase letters are lowered before the relevant colour called.

--- pyscript/base.py
+++ pyscript/base.py
@@ -19,6 +19,7 @@ Base objects
 """
 import copy
 import types
+import string
 
 # -------------------------------------------------------------------------
 
@@ -142,12 +143,76 @@ class Color(PsObj):
             'magenta':(0,1,0,0),
             'yellow':(0,0,1,0),
             'black':(0,),
-            'white':(1,)}
+            'white':(1,),
+            'greenyellow':   (0.15,0,0.69,0),
+            'yellow':        (0,0,1,0),
+            'goldenrod':     (0,0.10,0.84,0),
+            'dandelion':     (0,0.29,0.84,0),
+            'apricot':       (0,0.32,0.52,0),
+            'peach':         (0,0.50,0.70,0),
+            'melon':         (0,0.46,0.50,0),
+            'yelloworange':  (0,0.42,1,0),
+            'orange':        (0,0.61,0.87,0),
+            'burntorange':   (0,0.51,1,0),
+            'bittersweet':   (0,0.75,1,0.24),
+            'redorange':     (0,0.77,0.87,0),
+            'mahogany':      (0,0.85,0.87,0.35),
+            'maroon':        (0,0.87,0.68,0.32),
+            'brickred':      (0,0.89,0.94,0.28),
+            'orangered':     (0,1,0.50,0),
+            'rubinered':     (0,1,0.13,0),
+            'wildstrawberry':(0,0.96,0.39,0),
+            'salmon':        (0,0.53,0.38,0),
+            'carnationpink': (0,0.63,0,0),
+            'magenta':       (0,1,0,0),
+            'violetred':     (0,0.81,0,0),
+            'rhodamine':     (0,0.82,0,0),
+            'mulberry':      (0.34,0.90,0,0.02),
+            'redviolet':     (0.07,0.90,0,0.34),
+            'fuchsia':       (0.47,0.91,0,0.08),
+            'lavender':      (0,0.48,0,0),
+            'thistle':       (0.12,0.59,0,0),
+            'orchid':        (0.32,0.64,0,0),
+            'darkorchid':    (0.40,0.80,0.20,0),
+            'purple':        (0.45,0.86,0,0),
+            'plum':          (0.50,1,0,0),
+            'violet':        (0.79,0.88,0,0),
+            'royalpurple':   (0.75,0.90,0,0),
+            'blueviolet':    (0.86,0.91,0,0.04),
+            'periwinkle':    (0.57,0.55,0,0),
+            'cadetblue':     (0.62,0.57,0.23,0),
+            'cornflowerblue':(0.65,0.13,0,0),
+            'midnightblue':  (0.98,0.13,0,0.43),
+            'navyblue':      (0.94,0.54,0,0),
+            'royalblue':     (1,0.50,0,0),
+            'cerulean':      (0.94,0.11,0,0),
+            'cyan':          (1,0,0,0),
+            'processblue':   (0.96,0,0,0),
+            'skyblue':       (0.62,0,0.12,0),
+            'turquoise':     (0.85,0,0.20,0),
+            'tealblue':      (0.86,0,0.34,0.02),
+            'aquamarine':    (0.82,0,0.30,0),
+            'bluegreen':     (0.85,0,0.33,0),
+            'emerald':       (1,0,0.50,0),
+            'junglegreen':   (0.99,0,0.52,0),
+            'seagreen':      (0.69,0,0.50,0),
+            'forestgreen':   (0.91,0,0.88,0.12),
+            'pinegreen':     (0.92,0,0.59,0.25),
+            'limegreen':     (0.50,0,1,0),
+            'yellowgreen':   (0.44,0,0.74,0),
+            'springgreen':   (0.26,0,0.76,0),
+            'olivegreen':    (0.64,0,0.95,0.40),
+            'rawsienna':     (0,0.72,1,0.45),
+            'sepia':         (0,0.83,1,0.70),
+            'brown':         (0,0.81,1,0.60),
+            'tan':           (0.14,0.42,0.56,0),
+            'gray':          (0,0,0,0.50),
+            }
 
     def __init__(self,*col,**dict):
       
         if type(col[0])==types.StringType:
-            col=self.COLORS[col[0]]
+            col=self.COLORS[string.lower(col[0])]
 
         # some sanity checks
         assert len(col)>0 and len(col)<5
