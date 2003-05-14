2d1e6d8367f28e93b0539c76271ddd33a5876343
Paul Cochrane <paul@liekut.de>
2003-05-14 05:14:31 +0000

    Expanded the number of tests in the electronics library to include checking the rotations of the elements.  Now generates an eps file that lists and displays the objects tested.

--- admin/lib_test_suite.py
+++ admin/lib_test_suite.py
@@ -55,14 +55,62 @@ if testLib == 'electronics' or testLib == 'all':
 	from pyscript.lib.electronics import *
 	print "Testing %s library routines" % library
 	
-	funcs = [ 'AndGate()',
-          	'NandGate()',
-          	'OrGate()',
-          	'NorGate()',
-          	'XorGate()',
-          	'NxorGate()' ]
+	funcs = [ 
+		r"AndGate()",
+		r"AndGate(direction='e')",
+		r"AndGate(direction='w')",
+		r"AndGate(direction='s')",
+		r"AndGate(direction='n')",
+		r"AndGate(direction='moo')",
+          	r"NandGate()",
+          	r"NandGate(direction='e')",
+          	r"NandGate(direction='w')",
+          	r"NandGate(direction='s')",
+          	r"NandGate(direction='n')",
+          	r"NandGate(direction='moo')",
+          	r"OrGate()",
+          	r"OrGate(direction='e')",
+          	r"OrGate(direction='w')",
+          	r"OrGate(direction='s')",
+          	r"OrGate(direction='n')",
+          	r"OrGate(direction='moo')",
+          	r"NorGate()",
+          	r"NorGate(direction='e')",
+          	r"NorGate(direction='w')",
+          	r"NorGate(direction='s')",
+          	r"NorGate(direction='n')",
+          	r"NorGate(direction='moo')",
+          	r"XorGate()",
+          	r"XorGate(direction='e')",
+          	r"XorGate(direction='w')",
+          	r"XorGate(direction='s')",
+          	r"XorGate(direction='n')",
+          	r"XorGate(direction='moo')",
+          	r"NxorGate()",
+          	r"NxorGate(direction='e')",
+          	r"NxorGate(direction='w')",
+          	r"NxorGate(direction='s')",
+          	r"NxorGate(direction='n')",
+          	r"NxorGate(direction='moo')",
+          	r"NotGate()",
+          	r"NotGate(direction='e')",
+          	r"NotGate(direction='w')",
+          	r"NotGate(direction='s')",
+          	r"NotGate(direction='n')",
+          	r"NotGate(direction='moo')",
+		r"Resistor()",
+		r"Resistor(direction='ew')",
+		r"Resistor(direction='ns')",
+		r"Resistor(direction='moo')",
+		r"Capacitor()",
+		r"Capacitor(direction='ew')",
+		r"Capacitor(direction='ns')",
+		r"Capacitor(direction='moo')",
+		 ]
 	
+	electronicsObjs = Group()
 	for i in range(len(funcs)):
+		print "\tTesting %s" % funcs[i]
 		try:
         		obj = eval(funcs[i])
 		except Exception, e:
@@ -72,10 +120,15 @@ if testLib == 'electronics' or testLib == 'all':
         	try:
 			fname = funcs[i] + '.eps'
                 	render(obj,file=fname)
+			obj.move(0,3.2*i)
+			electronicsObjs.append(obj)
+			electronicsObjs.append(Text(nw=obj.sw-P(0,0.2),text=funcs[i]))
         	except:
                 	print "\tEeek!  Couldn't render %s" % funcs[i]
         	else:
                 	print "\tOk, %s seems to work" % funcs[i]
+
+	render(electronicsObjs,file="electronicsLib.eps")
 	
 	print "done %s library\n" % library
 
