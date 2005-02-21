b20201166afe582ea8631d2d7c8b9bc96fb8f2ae
Paul Cochrane <paul@liekut.de>
2005-02-21 00:41:38 +0000

    Many code tidying changes.  I'll try and do this on a file-by-file basis, so that any bugs I might have introduced are found.
    Removed use of string module.  Changed 'dict' variable to be called 'options'
    (as that is what it refers to, and dict is a python internal word.  Fixed
    calls to raise; they need an error type as the first argument.  Changed
    (possibly incorrectly) some of the calls to the __init__ method of the
    superclass to no longer use the apply() method.

--- pyscript/base.py
+++ pyscript/base.py
@@ -24,7 +24,7 @@ __revision__ = '$Revision$'
 
 import copy
 import types
-import string
+#from pyscript.vectors import Bbox
 
 # -------------------------------------------------------------------------
 class PyScriptError(Exception):
@@ -54,13 +54,14 @@ class PsObj(object):
     Base Class that most pyscript objects should subclass
     """
 
-    def __init__(self, **dict):
+    def __init__(self, **options):
         '''
         can pass a dict of atributes to set
         '''
-        apply(self, (), dict)
+        #object.__init__(self)
+        apply(self, (), options)
 
-    def __call__(self, **dict):
+    def __call__(self, **options):
         '''
         Set a whole lot of attributes in one go
         
@@ -75,7 +76,7 @@ class PsObj(object):
         # this will raise an exception if class doesn't have attribute
         # I think this is good.
         prop = []
-        for key, value in dict.items():
+        for key, value in options.items():
             if isinstance(eval('self.__class__.%s'%key), property):
                 prop.append((key, value))
             else:
@@ -91,7 +92,7 @@ class PsObj(object):
         return self
         
 
-    def copy(self, **dict):
+    def copy(self, **options):
         '''
         return a copy of this object
         with listed attributes modified
@@ -103,7 +104,7 @@ class PsObj(object):
         # here for convenience
         obj = copy.deepcopy(self)
 
-        apply(obj, (), dict)
+        apply(obj, (), options)
 
         return obj
     
@@ -209,139 +210,140 @@ class Color(PsObj):
         "darkred":(139, 0, 0),
         "darksalmon":(233, 150, 122),
         "darkseagreen":(143, 188, 143),
-        "darkslateblue":(72,61,139),
-        "darkslategray":(47,79,79),
-        "darkslategrey":(47,79,79),
-        "darkturquoise":(0,206,209),
-        "darkviolet":(148,0,211),
-        "deeppink":(255,20,147),
-        "deepskyblue":(0,191,255),
-        "dimgray":(105,105,105),
-        "dimgrey":(105,105,105),
-        "dodgerblue":(30,144,255),
-        "firebrick":(178,34,34),
-        "floralwhite":(255,250,240),
-        "forestgreen":(34,139,34),
-        "fuchsia":(255,0,255),
-        "gainsboro":(220,220,220),
-        "ghostwhite":(248,248,255),
-        "gold":(255,215,0),
-        "goldenrod":(218,165,32),
-        "gray":(128,128,128),
-        "grey":(128,128,128),
-        "green":(0,128,0),
-        "greenyellow":(173,255,47),
-        "honeydew":(240,255,240),
-        "hotpink":(255,105,180),
-        "indianred":(205,92,92),
-        "indigo":(75,0,130),
-        "ivory":(255,255,240),
-        "khaki":(240,230,140),
-        "lavender":(230,230,250),
-        "lavenderblush":(255,240,245),
-        "lawngreen":(124,252,0),
-        "lemonchiffon":(255,250,205),
-        "lightblue":(173,216,230),
-        "lightcoral":(240,128,128),
-        "lightcyan":(224,255,255),
-        "lightgoldenrod":(250,250,210),
-        "lightgreen":(144,238,144),
-        "lightgray":(211,211,211),
-        "lightgrey":(211,211,211),
-        "lightpink":(255,182,193),
-        "lightsalmon":(255,160,122),
-        "lightseagreen":(32,178,170),
-        "lightskyblue":(135,206,250),
-        "lightslategray":(119,136,153),
-        "lightslategrey":(119,136,153),
-        "lightsteelblue":(176,196,222),
-        "lightyellow":(255,255,224),
-        "lime":(0,255,0),
-        "limegreen":(50,205,50),
-        "linen":(250,240,230),
-        "magenta":(255,0,255),
-        "maroon":(128,0,0),
-        "mediumaquamarine":(102,205,170),
-        "mediumblue":(0,0,205),
-        "mediumorchid":(186,85,211),
-        "mediumpurple":(147,112,219),
-        "mediumseagreen":(60,179,113),
-        "mediumslateblue":(123,104,238),
-        "mediumspringgreen":(0,250,154),
-        "mediumturquoise":(72,209,204),
-        "mediumvioletred":(199,21,133),
-        "midnightblue":(25,25,112),
-        "mintcream":(245,255,250),
-        "mistyrose":(255,228,225),
-        "moccasin":(255,228,181),
-        "navajowhite":(255,222,173),
-        "navy":(0,0,128),
-        "oldlace":(253,245,230),
-        "olive":(128,128,0),
-        "olivedrab":(107,142,35),
-        "orange":(255,165,0),
-        "orangered":(255,69,0),
-        "orchid":(218,112,214),
-        "palegoldenrod":(238,232,170),
-        "palegreen":(152,251,152),
-        "paleturquoise":(175,238,238),
-        "palevioletred":(219,112,147),
-        "papayawhip":(255,239,213),
-        "peachpuff":(255,218,185),
-        "peru":(205,133,63),
-        "pink":(255,192,203),
-        "plum":(221,160,221),
-        "powderblue":(176,224,230),
-        "purple":(128,0,128),
-        "red":(255,0,0),
-        "rosybrown":(188,143,143),
-        "royalblue":(65,105,225),
-        "saddlebrown":(139,69,19),
-        "salmon":(250,128,114),
-        "sandybrown":(244,164,96),
-        "seagreen":(46,139,87),
-        "seashell":(255,245,238),
-        "sienna":(160,82,45),
-        "silver":(192,192,192),
-        "skyblue":(135,206,235),
-        "slateblue":(106,90,205),
-        "slategray":(112,128,144),
-        "slategrey":(112,128,144),
-        "snow":(255,250,250),
-        "springgreen":(0,255,127),
-        "steelblue":(70,130,180),
-        "tan":(210,180,140),
-        "teal":(0,128,128),
-        "thistle":(216,191,216),
-        "tomato":(255,99,71),
-        "turquoise":(64,224,208),
-        "violet":(238,130,238),
-        "wheat":(245,222,179),
-        "white":(255,255,255),
-        "whitesmoke":(245,245,245),
-        "yellow":(255,255,0),
-        "yellowgreen":(154,205,50),
+        "darkslateblue":(72, 61, 139),
+        "darkslategray":(47, 79, 79),
+        "darkslategrey":(47, 79, 79),
+        "darkturquoise":(0, 206, 209),
+        "darkviolet":(148, 0, 211),
+        "deeppink":(255, 20, 147),
+        "deepskyblue":(0, 191, 255),
+        "dimgray":(105, 105, 105),
+        "dimgrey":(105, 105, 105),
+        "dodgerblue":(30, 144, 255),
+        "firebrick":(178, 34, 34),
+        "floralwhite":(255, 250, 240),
+        "forestgreen":(34, 139, 34),
+        "fuchsia":(255, 0, 255),
+        "gainsboro":(220, 220, 220),
+        "ghostwhite":(248, 248, 255),
+        "gold":(255, 215, 0),
+        "goldenrod":(218, 165, 32),
+        "gray":(128, 128, 128),
+        "grey":(128, 128, 128),
+        "green":(0, 128, 0),
+        "greenyellow":(173, 255, 47),
+        "honeydew":(240, 255, 240),
+        "hotpink":(255, 105, 180),
+        "indianred":(205, 92, 92),
+        "indigo":(75, 0, 130),
+        "ivory":(255, 255, 240),
+        "khaki":(240, 230, 140),
+        "lavender":(230, 230, 250),
+        "lavenderblush":(255, 240, 245),
+        "lawngreen":(124, 252, 0),
+        "lemonchiffon":(255, 250, 205),
+        "lightblue":(173, 216, 230),
+        "lightcoral":(240, 128, 128),
+        "lightcyan":(224, 255, 255),
+        "lightgoldenrod":(250, 250, 210),
+        "lightgreen":(144, 238, 144),
+        "lightgray":(211, 211, 211),
+        "lightgrey":(211, 211, 211),
+        "lightpink":(255, 182, 193),
+        "lightsalmon":(255, 160, 122),
+        "lightseagreen":(32, 178, 170),
+        "lightskyblue":(135, 206, 250),
+        "lightslategray":(119, 136, 153),
+        "lightslategrey":(119, 136, 153),
+        "lightsteelblue":(176, 196, 222),
+        "lightyellow":(255, 255, 224),
+        "lime":(0, 255, 0),
+        "limegreen":(50, 205, 50),
+        "linen":(250, 240, 230),
+        "magenta":(255, 0, 255),
+        "maroon":(128, 0, 0),
+        "mediumaquamarine":(102, 205, 170),
+        "mediumblue":(0, 0, 205),
+        "mediumorchid":(186, 85, 211),
+        "mediumpurple":(147, 112, 219),
+        "mediumseagreen":(60, 179, 113),
+        "mediumslateblue":(123, 104, 238),
+        "mediumspringgreen":(0, 250, 154),
+        "mediumturquoise":(72, 209, 204),
+        "mediumvioletred":(199, 21, 133),
+        "midnightblue":(25, 25, 112),
+        "mintcream":(245, 255, 250),
+        "mistyrose":(255, 228, 225),
+        "moccasin":(255, 228, 181),
+        "navajowhite":(255, 222, 173),
+        "navy":(0, 0, 128),
+        "oldlace":(253, 245, 230),
+        "olive":(128, 128, 0),
+        "olivedrab":(107, 142, 35),
+        "orange":(255, 165, 0),
+        "orangered":(255, 69, 0),
+        "orchid":(218, 112, 214),
+        "palegoldenrod":(238, 232, 170),
+        "palegreen":(152, 251, 152),
+        "paleturquoise":(175, 238, 238),
+        "palevioletred":(219, 112, 147),
+        "papayawhip":(255, 239, 213),
+        "peachpuff":(255, 218, 185),
+        "peru":(205, 133, 63),
+        "pink":(255, 192, 203),
+        "plum":(221, 160, 221),
+        "powderblue":(176, 224, 230),
+        "purple":(128, 0, 128),
+        "red":(255, 0, 0),
+        "rosybrown":(188, 143, 143),
+        "royalblue":(65, 105, 225),
+        "saddlebrown":(139, 69, 19),
+        "salmon":(250, 128, 114),
+        "sandybrown":(244, 164, 96),
+        "seagreen":(46, 139, 87),
+        "seashell":(255, 245, 238),
+        "sienna":(160, 82, 45),
+        "silver":(192, 192, 192),
+        "skyblue":(135, 206, 235),
+        "slateblue":(106, 90, 205),
+        "slategray":(112, 128, 144),
+        "slategrey":(112, 128, 144),
+        "snow":(255, 250, 250),
+        "springgreen":(0, 255, 127),
+        "steelblue":(70, 130, 180),
+        "tan":(210, 180, 140),
+        "teal":(0, 128, 128),
+        "thistle":(216, 191, 216),
+        "tomato":(255, 99, 71),
+        "turquoise":(64, 224, 208),
+        "violet":(238, 130, 238),
+        "wheat":(245, 222, 179),
+        "white":(255, 255, 255),
+        "whitesmoke":(245, 245, 245),
+        "yellow":(255, 255, 0),
+        "yellowgreen":(154, 205, 50),
         }
 
 
-    def __init__(self, *col, **dict):
+    def __init__(self, *col, **options):
         """
         Initialisation of the colour object
 
         @param col:
         @type col:
 
-        @param dict:
-        @type dict:
+        @param options:
+        @type options:
         """
+
         # some sanity checks
         if type(col[0]) == types.StringType:
             
             if col[0][0] == "#" and len(col[0]) == 7:
                 # hex color scheme  eg #A0FF00
-                col = (string.atoi(col[0][1:3], 16) / 255.0,
-                     string.atoi(col[0][3:5], 16) / 255.0,
-                     string.atoi(col[0][5:7], 16) / 255.0)
+                col = (int(col[0][1:3], 16) / 255.0,
+                     int(col[0][3:5], 16) / 255.0,
+                     int(col[0][5:7], 16) / 255.0)
             else:
                 # named color
                 col = col[0].lower()
@@ -349,12 +351,14 @@ class Color(PsObj):
                 # renormalise so that values are in [0,1]
                 col = (col[0]/255., col[1]/255., col[2]/255.)
             
-        assert len(col)>0 and len(col)<5
-        for ii in col: assert ii >= 0 and ii <= 1
+        assert len(col) > 0 and len(col) < 5
+        for ii in col: 
+            assert ii >= 0 and ii <= 1
         
         self.color = col
             
-        apply(PsObj.__init__, (self, ), dict)
+        #apply(PsObj.__init__, (self, ), options)
+        PsObj.__init__(self)
         
         
     def body(self):
@@ -365,15 +369,15 @@ class Color(PsObj):
         color = self.color
         if len(color) == 1:
             # grayscale color
-            ps = " %g setgray "%color
+            ps = " %g setgray " % color
         elif len(color) == 3:
             # rgb color
-            ps = " %g %g %g setrgbcolor "%color
+            ps = " %g %g %g setrgbcolor " % color
         elif len(color) == 4:
             # cmyk color
-            ps = " %g %g %g %g setcmykcolor "%color
+            ps = " %g %g %g %g setcmykcolor " % color
         else:
-            raise "Unknown color"
+            raise ValueError, "Unknown color"
         return ps
     
     def __mul__(self, other):
@@ -411,25 +415,32 @@ class Dash(PsObj):
 
     """
 
-    pattern=(2,)
-    offset=0
+    pattern = (2, )
+    offset = 0
 
-    def __init__(self,*args,**dict):
+    def __init__(self, *args, **options):
+        """
+        Initialisation of the Dash object
+        """
 
         if len(args) > 0:
-            self.pattern=args
+            self.pattern = args
      
-        PsObj.__init__(self,**dict)
+        PsObj.__init__(self, **options)
        
        
     def body(self):
+        """
+        Returns the postscript of a Dash object
+        """
 
         # if we don't start with a space horrible things happen
     
-        pat=""
-        pat = "[ "+ string.join(["%g" % l for l in self.pattern],' ')    
+        pat = ""
+        delim = ' '
+        pat = "[ "+ delim.join(["%g" % l for l in self.pattern])    
 
-        pat = pat+"] %g setdash "%self.offset
+        pat = pat + "] %g setdash " % self.offset
 
         return pat
 
