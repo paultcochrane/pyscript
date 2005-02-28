9cee7090b46d64da00cffeec67a36fa4b3361dd9
Paul Cochrane <paul@liekut.de>
2005-02-28 00:43:21 +0000

    Initial import to repository.  This implements the PyScriptTest class, used in testing the output of the pyscript objects (see doc/examples/test_examples).

---
+++ pyscript/tests/pyscriptTest.py
@@ -0,0 +1,27 @@
+# $Id$
+
+from pyscript.groups import Group, VAlign, HAlign
+from pyscript.objects import Text
+
+class PyScriptTest(Group):
+    def __init__(self, **options):
+        Group.__init__(self, **options)
+        self.text = "undecorated"
+        self.objects = VAlign()
+
+    def test(self, object, text="undecorated"):
+        """
+        Test the given object, using the text to indicate what is being
+        tested
+        """
+        if object is None:
+            raise ValueError, "I need an object!!"
+
+        if text is not None:
+            self.text = text
+
+        self.objects.append( HAlign( object, Text(self.text) ) )
+	print "Appending %s" % object.__class__
+
+        return
+
