fbf03458b320a1aad3b3f8b56c28e37ee48ac322
Alexei Gilchrist <alexei.gilchrist@mq.edu.au>
2005-02-28 00:11:53 +0000

    updated to use new classes

--- doc/examples/arrow.py
+++ doc/examples/arrow.py
@@ -2,17 +2,11 @@
 
 from pyscript import *
 
-p=Path(P(0,0),P(1,1))
-
-g=Group(p,
-    Arrowhead1(tip=p.P( 1),angle=45),
-    Arrowhead2(tip=p.P(.8),angle=45),
-    Arrowhead3(tip=p.P(.6),angle=45),
-    Arrowhead4(tip=p.P(.4),angle=45)
-    ).scale(5)
+p=Path(P(0,0),C(20,200),P(4,4),
+	heads=[Arrowhead(1), Arrowhead2(.8), Arrowhead3(.6), Arrowhead4(.4)])
 
 render(
-    g,
+    p,
     file='arrow.eps',
     )
 
