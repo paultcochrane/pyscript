4bc88b2a3596c6c1b61b7168151835d1b47f6c17
Paul Cochrane <paul@liekut.de>
2005-02-28 00:45:18 +0000

    Reimplemented to use the PyScriptTest class.

--- doc/examples/test_examples/test_circle.py
+++ doc/examples/test_examples/test_circle.py
@@ -6,23 +6,34 @@
 __revision__ = '$Revision$'
 
 from pyscript import *
+import sys
+sys.path.append('../../../pyscript/tests/')
+from pyscriptTest import PyScriptTest
 
 defaults.units=UNITS['cm']
 
-circles = []
-circles.append( Circle() )       # should be radius=1cm, black fg, None bg
-circles.append( Circle(r=2.0) )  # should be radius=2cm, black fg, None bg
-circles.append( Circle(linewidth=2) )
-circles.append( Circle(bg=Color("red")) )  # radius=1cm, red bg, black fg
-circles.append( Circle(fg=Color("blue")) ) # radius=1cm, blue fg, None bg
-circles.append( Circle(start=90) )
-circles.append( Circle(end=90) )
-circles.append( Circle(start=90, end=270) )
-circles.append( Circle(start=15.2, end=67.9) )
+circles = PyScriptTest()
 
-i = 0
-for circle in circles:
-    i = i + 1
-    render(circle, file="test_circle%d.eps" % i)
+# test init
+circles.test( Circle() )       # should be radius=1cm, black fg, None bg
+
+# test radius
+circles.test( Circle(r=2.0), "r=2.0" )  # should be radius=2cm, black fg, None bg
+
+# test linewidth
+circles.test( Circle(linewidth=2), "linewidth=2" )
+
+# test colours
+circles.test( Circle(bg=Color("red")), "bg = red" )  # radius=1cm, red bg, black fg
+circles.test( Circle(fg=Color("blue")), "fg = blue" ) # radius=1cm, blue fg, None bg
+
+# test starting and ending
+circles.test( Circle(start=90), "start=90" )
+circles.test( Circle(end=90), "end=90" )
+circles.test( Circle(start=90, end=270), "start=90, end=270" )
+circles.test( Circle(start=15.2, end=67.9), "start=15.2, end=67.9" )
+
+# render them
+render(circles, file="test_circle.eps")
 
 # vim: expandtab shiftwidth=4:
