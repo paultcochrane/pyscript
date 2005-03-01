47882ab32bc4e3f746b54d1aa3a20da7278899e7
Paul Cochrane <paul@liekut.de>
2005-03-01 10:12:11 +0000

    Changed arrowhead calls to match updated capitalisation in pyscript.

--- doc/examples/test_examples/test_arrowhead.py
+++ doc/examples/test_examples/test_arrowhead.py
@@ -15,47 +15,47 @@ defaults.units=UNITS['cm']
 arrowheads  = PyScriptTest()
 
 # test init
-arrowheads.test( Arrowhead(), "Arrowhead" ) 
+arrowheads.test( ArrowHead(), "Arrowhead" ) 
 
 # test internal scaling
-arrowheads.test( Arrowhead(scalew=10, scaleh=10), "internal scale 10,10") 
-arrowheads.test( Arrowhead(scalew=10, scaleh=5), "internal scale 10,5")
-arrowheads.test( Arrowhead(scalew=5, scaleh=10), "internal scale 5,10")
+arrowheads.test( ArrowHead(scalew=10, scaleh=10), "internal scale 10,10") 
+arrowheads.test( ArrowHead(scalew=10, scaleh=5), "internal scale 10,5")
+arrowheads.test( ArrowHead(scalew=5, scaleh=10), "internal scale 5,10")
 # one would think this one should make it barf eh?
-arrowheads.test( Arrowhead(scalew=-5, scaleh=-5), "internal scale -5,-5")
+arrowheads.test( ArrowHead(scalew=-5, scaleh=-5), "internal scale -5,-5")
 # test external scaling
-arrowheads.test( Arrowhead().scale(10), "external scale 10")
-arrowheads.test( Arrowhead().scale(10,10), "external scale 10,10")
+arrowheads.test( ArrowHead().scale(10), "external scale 10")
+arrowheads.test( ArrowHead().scale(10,10), "external scale 10,10")
 
 # now add the scale option to each so can see them
-arrowheads.test( Arrowhead(linewidth=2).scale(10), "linewidth=2")
-arrowheads.test( Arrowhead(bg=Color("red")).scale(10), "bg = red")
-arrowheads.test( Arrowhead(fg=Color("blue")).scale(10), "fg = blue")
+arrowheads.test( ArrowHead(linewidth=2).scale(10), "linewidth=2")
+arrowheads.test( ArrowHead(bg=Color("red")).scale(10), "bg = red")
+arrowheads.test( ArrowHead(fg=Color("blue")).scale(10), "fg = blue")
 
 # test angles
 # should point north
-arrowheads.test( Arrowhead(angle=0).scale(10), "angle=0")  
+arrowheads.test( ArrowHead(angle=0).scale(10), "angle=0")  
 # should point east
-arrowheads.test( Arrowhead(angle=90).scale(10), "angle=90") 
+arrowheads.test( ArrowHead(angle=90).scale(10), "angle=90") 
 # should point south
-arrowheads.test( Arrowhead(angle=180).scale(10), "angle=180") 
+arrowheads.test( ArrowHead(angle=180).scale(10), "angle=180") 
 # should point west
-arrowheads.test( Arrowhead(angle=270).scale(10), "angle=270") 
+arrowheads.test( ArrowHead(angle=270).scale(10), "angle=270") 
 # should also point west
-arrowheads.test( Arrowhead(angle=-90).scale(10), "angle=-90") 
+arrowheads.test( ArrowHead(angle=-90).scale(10), "angle=-90") 
 # make sure can handle floats
-arrowheads.test( Arrowhead(angle=83.71).scale(10), "angle=83.71") 
+arrowheads.test( ArrowHead(angle=83.71).scale(10), "angle=83.71") 
 
 # test the tip position
-arrowheads.test( Arrowhead(tip=P(0,0)).scale(10), "tip=P(0,0) default") 
+arrowheads.test( ArrowHead(tip=P(0,0)).scale(10), "tip=P(0,0) default") 
 # won't look much different on its own
-arrowheads.test( Arrowhead(tip=P(1.5,6.2)).scale(10), "tip=P(1.5,6.2)") 
+arrowheads.test( ArrowHead(tip=P(1.5,6.2)).scale(10), "tip=P(1.5,6.2)") 
 
 # test the other arrowheads
-arrowheads.test( Arrowhead1().scale(10), "Arrowhead1")
-arrowheads.test( Arrowhead2().scale(10), "Arrowhead2")
-arrowheads.test( Arrowhead3().scale(10), "Arrowhead2")
-arrowheads.test( Arrowhead4().scale(10), "Arrowhead4")
+arrowheads.test( ArrowHead1().scale(10), "ArrowHead1")
+arrowheads.test( ArrowHead2().scale(10), "ArrowHead2")
+arrowheads.test( ArrowHead3().scale(10), "ArrowHead2")
+arrowheads.test( ArrowHead4().scale(10), "ArrowHead4")
 
 # render them
 render(arrowheads, file="test_arrowhead.eps")
