aa55fa8a478f8e5cba827b19e7e193b1f3229997
Alexei Gilchrist <alexei.gilchrist@mq.edu.au>
2005-03-08 07:35:13 +0000

    added simple arrowhead test

---
+++ tests/test_arrowhead.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.insert(0,"../")
+
+from pyscript import *
+
+g=Group(
+	ArrowHead1(tip=P( 1,0),angle=10),
+	ArrowHead2(tip=P(.8,0),angle=30,bg=Color('yellow')),
+	ArrowHead3(tip=P(.6,0),angle=60,fg=Color('red')),
+	ArrowHead4(tip=P(.4,0),angle=90)
+	).scale(3)
+
+p=Path(P(0,0),C(90,-90),P(2,2),
+	heads=[ArrowHead(1),ArrowHead2(.8),ArrowHead3(.6),ArrowHead4(.4,reverse=1)])
+	
+render(
+	g,
+	p,
+	
+    file='test_arrowhead.eps',
+	
+    )
+
