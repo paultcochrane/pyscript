17eee0dbd986a6d3c9eb7090e02a8d5372b638f2
Alexei Gilchrist <alexei.gilchrist@mq.edu.au>
2005-03-09 01:21:13 +0000

    moved expected files into subdirectory (easier to copy and doesn't clutter the main dir) rewrote testpyscript.py ... should now be able to incoporate non-visual tests too

--- tests/testpyscript.py
+++ tests/testpyscript.py
@@ -3,17 +3,28 @@
 from pyscript import HAlign,Epsf,render
 import glob,os
 
+# clean up previous tests
+os.system('rm test_*.eps')
+
 tests=glob.glob('test_*.py')
 for test in tests:
-	basefile=os.path.splitext(test)[0]
-	expectfile=basefile+'_expect.eps'
-	if not os.path.exists(expectfile):
-		raise "%s doesn't exist"%expectfile
-
+	# this should generate a test_foo.eps for visual tests
+	# otherwise test something silently, but raise exception
+	# if it doesn't pass
 	os.system('pyscript %s'%test)
 
+# compare visual outputs
+samples=glob.glob('test_*.eps')
+for sample in samples:
+	expect=os.path.join('expected',sample)
+
+	# check expected/test_foo.eps exists otherwise nothing to compare to
+	if not os.path.exists(expect):
+		raise "%s doesn't exist"%expect
+
+	# put them side by side and display
 	render(
-		HAlign(Epsf(basefile+".eps"),Epsf(expectfile)),
+		HAlign(Epsf(sample),Epsf(expect)),
 		file='comparison.eps'
 		)
 	
