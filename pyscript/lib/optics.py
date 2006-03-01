1d9ba349a10898c873bc116eb00b92f043fc4688
Paul Cochrane <paul@liekut.de>
2006-03-01 13:25:33 +0000

    Removed label arguments as they can be done outside of the object.  This is cleaner, and more flexible.
    Added a BSLine object (there used to be a BSLine function, but I'd taken it
    out, and now I decided we needed one :-) )
    
    Improved design of the Modulator object shape.

--- pyscript/lib/optics.py
+++ pyscript/lib/optics.py
@@ -28,7 +28,8 @@ import types
 # beam splitter
 def BS(sw=P(0, 0), label=None, h=1.0):
     """
-    Beam splitter; displayed as a line
+    Beam splitter; displayed as a line possibly more useful in linear optics
+    quantum computation diagrams
 
     @param sw: location of the south-west corner of the object
     @type sw: L{P} object
@@ -73,9 +74,6 @@ class BSBox(Group):
     """
     Beam splitter as a box as opposed to a line
 
-    @ivar label: the label to attach to the beam splitter
-    @type label: C{string}, L{TeX} or L{Text} object
-
     @ivar height: height of the beam splitter (equal to its width)
     @type height: C{float}
 
@@ -89,7 +87,6 @@ class BSBox(Group):
     @type bg: L{Color} object
     """
 
-    label = None
     height = 1.0
     angle = 0.0  # not going to be used much (maybe for a Ralph-splitter ;-))
     fg = Color(0)
@@ -104,7 +101,6 @@ class BSBox(Group):
         self.bg = options.get("bg", self.bg)
         self.height = options.get("height", self.height)
         self.angle = options.get("angle", self.angle)
-        self.label = options.get("label", self.label)
 
         # make the beam splitter
         bs = Group()
@@ -120,49 +116,68 @@ class BSBox(Group):
         # rotate if necessary
         bs.rotate(self.angle, p=bs.bbox().c)
 
-        # put a label on if required
-        if self.label is not None:
-            # if the label is just a string, wrap it in a Text object
-            if type(self.label) is types.StringType:
-                self.label = Text(self.label)
-
-            # test to see if the label isn't a Text or a TeX object
-            if not isinstance(self.label, TeX) and \
-                    not isinstance(self.label, Text):
-                raise ValueError, \
-                        "label is not a string or Text or TeX object"
-
-            self.label.w = P(3.0*self.height/4.0, self.height/2.0)
-            bs.append(self.label)
-
         self.append(bs)
 
 # polarising beam splitter
 PBS = BSBox
 
 # line beam splitter
-#def BSLine(c=P(0,0), label=None, w=1.0, h=0.1, angle=0, anchor=None, 
-        #fg=Color(0), bg=Color(0)):
-    #"""
-    #Line beam splitter
-    #"""
-    #sw = c-P(w/2.0,h/2.0)
-    #beamSplitter = Path(sw, sw+P(0,h), sw+P(w,h), sw+P(w,0), sw,
-             #fg=fg, bg=bg)
-    #beamSplitter.rotate(angle,p=c)
-    #if label is not None:
-        #label.w = sw+P(h,0)+P(-h/4,h/2)
-        #return Group(beamSplitter, label)
-    #else:
-        #return Group(beamSplitter)
+class BSLine(Group):
+    """
+    Beam splitter as a line (i.e. a half-slivered mirror)
+
+    @ivar height: height of the beam splitter
+    @type height: float
+
+    @ivar thickness: thickness of the beam splitter
+    @type thickness: float
+
+    @ivar angle: rotation angle
+    @type angle: float
+
+    @ivar fg: foreground colour
+    @type fg: L{Color} object
+
+    @ivar bg: background colour
+    @type bg: L{Color} object
+    """
+
+    height = 1.0
+    thickness = 0.2
+    angle = 45.0
+    fg = Color(0)
+    bg = Color(1)
+
+    def __init__(self, **options):
+        # inherit from the base class
+        Group.__init__(self, **options)
+
+        # process the options if any
+        self.fg = options.get("fg", self.fg)
+        self.bg = options.get("bg", self.bg)
+        self.height = options.get("height", self.height)
+        self.thickness = options.get("thickness", self.thickness)
+        self.angle = options.get("angle", self.angle)
+
+        # make the beam splitter
+        bs = Group()
+        bs.append(Path(P(0, 0),
+                  P(0, self.height),
+                  P(self.thickness, self.height),
+                  P(self.thickness, 0),
+                  closed=1,
+                  fg=self.fg, bg=self.bg)
+                  )
+
+        # rotate if necessary
+        bs.rotate(self.angle, p=bs.bbox().c)
+
+        self.append(bs)
 
 # phase shifter
 class PhaseShifter(Group):
     """
     Phase shifter
-
-    @ivar label: phase shifter label
-    @type label: string
 
     @ivar width: phase shifter width
     @type width: float
@@ -180,7 +195,6 @@ class PhaseShifter(Group):
     @type bg: L{Color} object
     """
 
-    label = None
     width = 0.5
     height = 0.7
     angle = 0
@@ -192,7 +206,6 @@ class PhaseShifter(Group):
         Group.__init__(self, **options)
 
         # process the options if any
-        self.label = options.get("label", self.label)
         self.width = options.get("width", self.width)
         self.height = options.get("height", self.height)
         self.angle = options.get("angle", self.angle)
@@ -212,19 +225,12 @@ class PhaseShifter(Group):
         if self.angle != 0:
             ps.rotate(self.angle, p=ps.bbox().c)
 
-        if self.label is not None:
-            self.label.s = P(self.width/2.0, -self.height)
-            self.append(ps, self.label)
-        else:
-            self.append(ps)
+        self.append(ps)
 
 # mirror
 class Mirror(Group):
     """
     Mirror
-
-    @ivar label: mirror label
-    @type label: string, L{TeX} or L{Text} object
 
     @ivar length: mirror length
     @type length: float
@@ -245,7 +251,6 @@ class Mirror(Group):
     @type bg: L{Color} object
     """
 
-    label = None
     length = 1.0
     thickness = 0.1
     angle = 0.0
@@ -263,16 +268,15 @@ class Mirror(Group):
         self.length = options.get("length", self.length)
         self.thickness = options.get("thickness", self.thickness)
         self.angle = options.get("angle", self.thickness)
-        self.label = options.get("label", self.label)
         self.flicks = options.get("flicks", self.flicks)
 
         # make the mirror itself
         mirror = Group()
         mirror.append(
                       Path(P(0, 0), 
-                      P(0, self.thickness),
-                      P(self.length, self.thickness), 
-                      P(self.length, 0), 
+                      P(0, self.length),
+                      P(self.thickness, self.length), 
+                      P(self.thickness, 0), 
                       fg=self.fg, bg=self.bg, closed=1)
                       )
 
@@ -296,21 +300,6 @@ class Mirror(Group):
         # make the mirror the current object
         self.append(mirror)
 
-        # put a label on if required
-        if self.label is not None:
-            # if the label is just a string, wrap it in a Text object
-            if type(self.label) is types.StringType:
-                self.label = Text(self.label)
-
-            # test to see if the label isn't a Text or a TeX object
-            if not isinstance(self.label, TeX) and \
-                    not isinstance(self.label, Text):
-                raise ValueError, \
-                        "label is not a string or Text or TeX object"
-
-            self.label.c = self.bbox().c
-            self.append(self.label)
-
 # detector
 class Detector(Group):
     """
@@ -333,9 +322,6 @@ class Detector(Group):
 
     @ivar angle: rotation angle
     @type angle: float
-
-    @ivar label: detector label
-    @type label: string, L{TeX} or L{Text} object
     """
 
     height = 0.8
@@ -344,7 +330,6 @@ class Detector(Group):
     fg = Color(0)
     pad = 0.1
     angle = 0.0
-    label = None
 
     def __init__(self, **options):
         Group.__init__(self, **options)
@@ -378,32 +363,12 @@ class Detector(Group):
         self.angle = options.get("angle", self.angle)
         p.rotate(self.angle, p=p.bbox().c)
 
-        # put a label on if required
-        self.label = options.get("label", self.label)
-        if self.label is not None:
-            # if the label is just a string, wrap it in a Text object
-            if type(self.label) is types.StringType:
-                self.label = Text(self.label)
-
-            # test to see if the label isn't a Text or a TeX object
-            if not isinstance(self.label, TeX) and \
-                    not isinstance(self.label, Text):
-                raise ValueError, \
-                        "label is not a string or Text or TeX object"
-
-            # the location given here not tested!!!
-            self.label.w = P(3.0*self.height/4.0, self.height/2.0)
-            p.append(self.label)
-
         self.append(p)
 
 # laser (this is just a container, in case we want to make this fancier later)
 class Laser(Group):
     """
     Laser
-
-    @ivar label: laser label
-    @type label: string, L{TeX} or L{Text} object
 
     @ivar height: laser box height
     @type height: float
@@ -421,7 +386,6 @@ class Laser(Group):
     @type bg: L{Color} object
     """
 
-    label = None
     height = 1.0
     width = 3.0
     angle = 0.0
@@ -438,7 +402,6 @@ class Laser(Group):
         self.height = options.get("height", self.height)
         self.width = options.get("width", self.width)
         self.angle = options.get("angle", self.angle)
-        self.label = options.get("label", self.label)
 
         # make the laser
         laser = Group()
@@ -454,22 +417,6 @@ class Laser(Group):
         # rotate if necessary
         laser.rotate(self.angle, p=laser.bbox().c)
 
-        # put a label on if required
-        if self.label is not None:
-            # if the label is just a string, wrap it in a Text object
-            if type(self.label) is types.StringType:
-                self.label = Text(self.label)
-
-            # test to see if the label isn't a Text or a TeX object
-            if not isinstance(self.label, TeX) and \
-                    not isinstance(self.label, Text):
-                raise ValueError, \
-                        "label is not a string or Text or TeX object"
-
-            # put the label in an appropriate location
-            self.label.c = laser.bbox().c
-            laser.append(self.label)
-
         self.append(laser)
 
 # modulator 
@@ -477,9 +424,6 @@ class Laser(Group):
 class Modulator(Group):
     """
     Modulator (EOM, AOM etc.)
-
-    @ivar label: modulator label
-    @type label: string, L{TeX} or L{Text} object
 
     @ivar height: modulator box height
     @type height: float
@@ -497,12 +441,12 @@ class Modulator(Group):
     @type bg: L{Color} object
     """
 
-    label = None
-    height = 1.0
-    width = 0.5 
+    height = 0.5
+    width = 1.0 
     angle = 0.0
     fg = Color(0)
     bg = Color(1)
+    buf = height*0.2
 
     def __init__(self, **options):
         # inherit from the base class
@@ -514,7 +458,6 @@ class Modulator(Group):
         self.height = options.get("height", self.height)
         self.width = options.get("width", self.width)
         self.angle = options.get("angle", self.angle)
-        self.label = options.get("label", self.label)
 
         # make the laser
         modulator = Group()
@@ -525,27 +468,23 @@ class Modulator(Group):
                     P(self.width, self.height), 
                     P(self.width, 0), 
                     closed=1, 
-                    fg=self.fg, bg=self.bg
+                    fg=self.fg, bg=self.bg,
+                    ))
+        modulator.append(
+                Path(
+                    P(0, -self.buf),
+                    P(self.width, -self.buf),
+                    fg=self.fg, bg=self.bg,
+                    ))
+        modulator.append(
+                Path(
+                    P(0, self.height+self.buf),
+                    P(self.width, self.height+self.buf),
+                    fg=self.fg, bg=self.bg,
                     ))
 
         # rotate if necessary
         modulator.rotate(self.angle, p=modulator.bbox().c)
-
-        # put a label on if required
-        if self.label is not None:
-            # if the label is just a string, wrap it in a Text object
-            if type(self.label) is types.StringType:
-                self.label = Text(self.label)
-
-            # test to see if the label isn't a Text or a TeX object
-            if not isinstance(self.label, TeX) and \
-                    not isinstance(self.label, Text):
-                raise ValueError, \
-                        "label is not a string or Text or TeX object"
-
-            # put in appropriate location
-            self.label.c = modulator.bbox().c
-            modulator.append(self.label)
 
         self.append(modulator)
 
@@ -553,9 +492,6 @@ class Modulator(Group):
 class FreeSpace(Group):
     """
     A patch of free space (for example, in an interferometer)
-
-    @ivar label: label of free space region
-    @type label: string, L{TeX} or L{Text} object
 
     @ivar height: height of free space box
     @type height: float
@@ -573,7 +509,6 @@ class FreeSpace(Group):
     @type bg: L{Color} object
     """
 
-    label = None
     height = 1.0
     width = 3.0
     angle = 0.0
@@ -590,7 +525,6 @@ class FreeSpace(Group):
         self.height = options.get("height", self.height)
         self.width = options.get("width", self.width)
         self.angle = options.get("angle", self.angle)
-        self.label = options.get("label", self.label)
 
         # make the laser
         fs = Group()
@@ -607,22 +541,6 @@ class FreeSpace(Group):
 
         # rotate if necessary
         fs.rotate(self.angle, p=fs.bbox().c)
-
-        # put a label on if required
-        if self.label is not None:
-            # if the label is just a string, wrap it in a Text object
-            if type(self.label) is types.StringType:
-                self.label = Text(self.label)
-
-            # test to see if the label isn't a Text or a TeX object
-            if not isinstance(self.label, TeX) and \
-                    not isinstance(self.label, Text):
-                raise ValueError, \
-                        "label is not a string or Text or TeX object"
-
-            # put the label in appropriate location
-            self.label.c = fs.bbox().c
-            fs.append(self.label)
 
         self.append(fs)
 
