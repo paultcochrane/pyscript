29b7d5ca896587a4fd8f5bb20cc0ca3635f45b66
Paul Cochrane <paul@liekut.de>
2006-03-07 16:53:09 +0000

    Fixed Sagnac example so that it actually is a Sagnac interferometer.

--- doc/manual/optics/sagnac.py
+++ doc/manual/optics/sagnac.py
@@ -18,48 +18,42 @@ beam = Group()
 # the laser
 laser = Laser(c=P(0,0))
 
-# the "west" beam splitter
-bs_w = BSBox(height=0.7)
-bs_w.w = laser.e + P(1,0)
-beam.append(Path(laser.e, bs_w.w))
+# the beam splitter
+bs = BSBox(height=0.7)
+bs.w = laser.e + P(1,0)
+beam.append(Path(laser.e, bs.w))
 
 # the "north" mirror
 mirror_n = Mirror(angle=45)
-mirror_n.s = bs_w.n + P(0,3)
-beam.append(Path(bs_w.n, mirror_n.c))
+mirror_n.s = bs.n + P(0,2)
+beam.append(Path(bs.n, mirror_n.c))
 
 # the "east" mirror
 mirror_e = Mirror(angle=45)
-mirror_e.w = bs_w.e + P(3,0)
-beam.append(Path(bs_w.e, mirror_e.c))
+mirror_e.w = bs.e + P(3,0)
+beam.append(Path(bs.e, mirror_e.c))
 
-# the "east" beam splitter
-bs_e = BSBox(height=0.7)
-bs_e.c = P(mirror_e.c.x, mirror_n.c.y)
-beam.append(Path(mirror_e.c, bs_e.s))
-beam.append(Path(mirror_n.c, bs_e.w))
+# the "north-east" mirror
+mirror_ne = Mirror(angle=135)
+mirror_ne.c = P(mirror_e.c.x, mirror_n.c.y)
+beam.append(Path(mirror_n.c, mirror_ne.c))
+beam.append(Path(mirror_e.c, mirror_ne.c))
 
-# the "north" detector
-det_n = Detector(angle=-90)
-det_n.s = bs_e.n + P(0,1)
-beam.append(Path(bs_e.n, det_n.s))
-
-# the "east" detector
-det_e = Detector()
-det_e.w = bs_e.e + P(1,0)
-beam.append(Path(bs_e.e, det_e.w))
+# the detector
+det = Detector(angle=90)
+det.n = bs.s + P(0,-1)
+beam.append(Path(bs.s, det.n))
 
 # set the colour of the beam
 beam.apply(fg=Color("red"))
 
 # collect all the objects together
 fig = Group(
-        laser,
-        bs_w,
-        mirror_n, mirror_e,
-        bs_e,
-        det_n, det_e,
         beam,
+        laser,
+        bs,
+        mirror_n, mirror_e, mirror_ne,
+        det,
         )
 
 # render the figure
