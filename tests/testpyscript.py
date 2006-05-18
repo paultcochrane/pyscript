139a0e7b25075b99772fb803c388a460da682c66
Alexei Gilchrist <alexei.gilchrist@mq.edu.au>
2006-05-18 00:26:17 +0000

    o does a diff with expected file (ignoring creation date) o only shows comparison if diff fails o NB always fails for tex stuff cause dvips generates some random unique id's   each time ... bugger.

--- tests/testpyscript.py
+++ tests/testpyscript.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python
 
 from pyscript import HAlign,Epsf,render
-import glob,os
+import glob,os,commands
+
+
+def printit(test,message):
+    print test.ljust(70),message
 
 # clean up previous tests
 os.system('rm test_*.eps')
 
 tests=glob.glob('test_*.py')
 for test in tests:
-	# this should generate a test_foo.eps for visual tests
-	# otherwise test something silently, but raise exception
-	# if it doesn't pass
-	os.system('pyscript %s'%test)
+    # this should generate a test_foo.eps for visual tests
+    # otherwise test something silently, but raise exception
+    # if it doesn't pass
+    commands.getoutput('pyscript %s'%test)
 
-# compare visual outputs
-samples=glob.glob('test_*.eps')
-for sample in samples:
-	expect=os.path.join('expected',sample)
+    # for the moment assume all tests are visual and that there is 
+    # one eps file produced for each test with the same basename
+    sample,ext = os.path.splitext(os.path.basename(test))
+    sample=sample+".eps"
+    if not os.path.exists(sample):
+        printit(test,"no test file: %s"%sample)
+        continue
 
-	# check expected/test_foo.eps exists otherwise nothing to compare to
-	if not os.path.exists(expect):
-		raise "%s doesn't exist"%expect
+    expect=os.path.join('expected',sample)
 
-	# put them side by side and display
-	render(
-		HAlign(Epsf(sample),Epsf(expect)),
-		file='comparison.eps'
-		)
-	
-	os.system('gv comparison.eps')
+    # check expected/test_foo.eps exists otherwise nothing to compare to
+    if not os.path.exists(expect):
+        printit(test,"no expected file: %s"%expect)
+        continue
 
+    # there will be trivial changes to the EPS files 
+    # such as date stamp and pyscript version
+    output = commands.getoutput("diff -I '^%%%%CreationDate' %s %s "%(sample,expect))
+    if len(output)==0:
+        printit(test,"OK")
+        continue
 
+    # put them side by side and display
+    # XXX need a verbose switch on output
+    render(
+        HAlign(Epsf(sample),Epsf(expect)),
+        file='comparison.eps'
+        )
+    
+    os.system('gv comparison.eps')
 
 
