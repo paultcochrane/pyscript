b3f8d966576035ebb9da2bf86bf2f425d752ba4b
Paul Cochrane <paul@liekut.de>
2006-10-25 13:23:53 +0000

    Removed now defunct call to pyscript.lib.misc as the Arrow and ArrowHead objects are part of the base of pyscript.
    Removed some dead wood commented-out code.
    Changed the shebang line to use pyscript rather than python directly

--- doc/examples/xmdsProcess.py
+++ doc/examples/xmdsProcess.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env pyscript
 
 from pyscript import *
 from pyscript.lib.presentation import Box_1,TeXBox
-from pyscript.lib.misc import Arrow, ArrowHead
 
 defaults.tex_head+=r"\newcommand{\xmds}{\textsc{xmds}\xspace}"
-
-# class Box(Box_1):
-#     fg=Color('black')*.4
-#     bg=Color('LightGoldenRod')*1.1
-#     border=1
-#     pad=.2
-#     fixed_width = 6
 
 def Box(item, bg=Color('LightSteelBlue')*1.1):
     bg=bg
